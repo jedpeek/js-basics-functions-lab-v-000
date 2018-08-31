42568462b42d74c63834f4bf528242882e74e3b5
Jed Peek <jedpeek@yahoo.com>
2018-08-31 21:10:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function distanceFromHqInBlocks(block){
   const distance = 0;
   if (block >= 42){
-    return block - 42;
+    distance = block - 42;
   }
     else{
-      return 42 - block;
+    distance = 42 - block;
     }
     return distance;
 };
