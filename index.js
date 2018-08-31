55daf339089b2487000771fad5fd07f9148cece3
Jed Peek <jedpeek@yahoo.com>
2018-08-31 21:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ function distanceTravelledInFeet(a, b){
 
 function calculatesFarePrice(start, destination){
   let d = distanceTravelledInFeet(start, destination);
-  if (d < 400){
-    return 0;
-  }
-  else if(d < 2000){
-    return  (d - 400)*(.02)
-  }
-  else if (d > 2000 && d < 2500) {
-    return 25;
-  }
-  else {
-    return 'cannot travel that far'
-  }
+    if (d < 400){
+      return 0;
+    }
+    else if(d < 2000){
+      return  (d - 400)*(.02)
+    }
+    else if (d > 2000 && d < 2500) {
+      return 25;
+    }
+    else {
+      return 'cannot travel that far'
+    }
 
 }
