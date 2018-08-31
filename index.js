40f50095cd5e5690105073c0999e7153deb7f3d0
Jed Peek <jedpeek@yahoo.com>
2018-08-31 21:07:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(block){
+  let distance = 0;
   if (block >= 42){
     return block - 42
   }
     else{
       return 42 - block
     }
-}
+    return distance;
+};
+
+function distanceFromHqInFeet
